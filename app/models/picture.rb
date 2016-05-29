7bdbd9dc19d59d54828dcbb7e03fd83152822624
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-29 14:02:43 +0100

    image size

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,5 @@
 class Picture < ActiveRecord::Base
   validates :image, presence: true
-  has_attached_file :image
+  has_attached_file :image, styles: { :medium => "300" }
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-
 end
