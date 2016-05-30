74eac22a190e4c2334329b40978a4ae87f6dc2aa
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-30 13:47:23 +0100

    resized thumbnails to be square

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ActiveRecord::Base
   validates :image, :user_id, presence: true
   has_attached_file :image,
-                    styles: { thumbnail: '300', medium: '640' }
+                    styles: { thumbnail: '300x300#', medium: '640' }
 
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
